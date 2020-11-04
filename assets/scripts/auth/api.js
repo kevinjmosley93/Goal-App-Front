50d2137fefe1a980e569b6f503d4f7041c378f81
kevinjmosley93 <kevinjmosley93@gmail.com>
2020-11-03 22:22:23 -0600

    Latest Commit

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -2,6 +2,8 @@ const config = require("./../config");
 const store = require("../store");
 
 const signUp = (data) => {
+  store.user = data;
+
   console.log("api data is:", data);
   return $.ajax({
     url: `${config.apiUrl}/sign-up`,
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -7,7 +7,7 @@ const signUpClick = (event) => {
   event.preventDefault();
   const form = event.target;
   const data = getFormInfo(form);
-  api.signUp(data).then(ui.signUpPass);
+  api.signUp(data).then(ui.signUpPass).catch();
   console.log("data is:", data);
   console.log("event is:", event);
 };
