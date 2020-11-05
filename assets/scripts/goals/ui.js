d3336d312beeff2f62e51161a516ef260fb6c74b
kevinjmosley93 <kevinjmosley93@gmail.com>
2020-11-04 23:09:33 -0600

    Latest Commit

--- assets/scripts/goals/ui.js
+++ assets/scripts/goals/ui.js
@@ -2,6 +2,7 @@ const store = require("../store");
 
 const goalCreated = (res) => {
   store.user = res.user._id;
+  console.log("user is:", store.user);
   $("#message").text(`${store.user.email} is playing!`);
 };
 
