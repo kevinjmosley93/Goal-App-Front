58d9bdae98c95d33069c77b7572ec7e3da85ed45
kevinjmosley93 <kevinjmosley93@gmail.com>
2020-11-07 13:24:42 -0600

    Latest Commit

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl;
 const apiUrls = {
-  production: "https://track-a-goal-app.herokuapp.com/",
+  production: "https://track-a-goal-app.herokuapp.com",
   development: "http://localhost:4741",
 };
 
